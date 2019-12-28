c7f7fac24d00d0e5920e04b5afff0c6900ab0004
Tadas Janca <tadasjanca@gmail.com>
2019-12-28 21:35:37 +0200

    added random user seeder

---
+++ backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+//use Faker\Generator as Faker;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newUser                  = new User();
+        $newUser->name            = 'Admin';
+        $newUser->email           = 'admin@admin.com';
+        $newUser->password        = bcrypt('password');
+        $newUser->api_token       = Str::random(80);
+        $newUser->save();
+
+        $newUser                  = new User();
+        $newUser->name            = 'Tadas Janca';
+        $newUser->email           = 'tadasjanca@gmail.com';
+        $newUser->password        = bcrypt('password');
+        $newUser->api_token       = Str::random(80);
+        $newUser->save();
+
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            $newUser                  = new User();
+            $newUser->name            = $faker->name . ' ' . $faker->lastName;
+            $newUser->email           = $faker->email;
+            $newUser->password        = bcrypt('password');
+            $newUser->api_token       = Str::random(80);
+            $newUser->save();
+        }
+    }
+}
