2604d373e06ac56d1c62d06a51c4f8a77d351eb2
Tadas Janca <tadasjanca@gmail.com>
2020-01-20 11:57:38 +0200

    added base controller with response message methods, and refactored login method with passport

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -2,40 +2,49 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class LoginController extends BaseController
 {
-    //todo remake controller
-    //todo add CORS
 
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
     }
 
+    /**
+     * Login api
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function login()
     {
         $credentials = request(['email', 'password']);
-        $errorMsg = null;
+        $errorMsg = [];
 
-        if(!User::where('email', $credentials['email'])->first()) {
-            $errorMsg = "Email \"{$credentials['email']}\" does  not exist";
-        }
+        if(Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')-> accessToken;
+            $success['name'] =  $user->name;
 
-        if (!$errorMsg  && !auth()->attempt($credentials)) {
-            $errorMsg = "Mismatched password for email \"{$credentials['email']}\"";
-        }
+            return $this->sendResponse($success, 'You have successfully logged in, ' . auth()->user()->name);
+        } else{
 
-        if ($errorMsg ) {
-            return response()->json(['error' => $errorMsg], 401);
-        } else {
-            return response()->json([
-                'success' => 'You have successfully logged in, ' . auth()->user()->name ,
-                'api_token' => auth()->user()->api_token],
-                200);
+            if(!User::where('email', $credentials['email'])->first()) {
+
+                $errorMsg['email'] = "Email '{$credentials['email']}' does not exist";
+
+             } else if (!auth()->attempt($credentials)) {
+
+                $errorMsg['password'] = "Mismatched password for email '{$credentials['email']}'";
+            }
+
+            return $this->sendError('Unauthorised.', $errorMsg);
         }
     }
 }
