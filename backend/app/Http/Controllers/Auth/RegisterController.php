0e5bdf88b644b11383a87247b97f1b2cfd6ec5b4
Tadas Janca <tadasjanca@gmail.com>
2020-01-20 13:55:19 +0200

    removed api_token from users table and removed unnecessary code lines

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -3,45 +3,18 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\BaseController;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\RegisterRequest;
 use App\Http\Validators\AuthValidator;
 use App\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 
 class RegisterController extends BaseController
 {
-    //todo remake controller
 
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['register']]);
     }
-
-//    public function register(RegisterRequest $request)
-//    {
-//        $user = $this->create($request);
-//        return response()->json([
-//            'success' => 'User with email ' . $user->email . ' was successfully registered',
-//            'api_token' => $user->api_token
-//        ], 200);
-//    }
-//
-//    protected function create($data)
-//    {
-//        $newUser            = new User();
-//        $newUser->name      = $data['name'];
-//        $newUser->email     = $data['email'];
-//        $newUser->password  = Hash::make($data['password']);
-//        $newUser->api_token = Str::random(80);
-//        $newUser->save();
-//
-//        return $newUser;
-//    }
 
     /**
      * Register api
@@ -63,6 +36,7 @@ class RegisterController extends BaseController
         $success['token'] =  $user->createToken('MyApp')->accessToken;
         $success['name'] =  $user->name;
         $message = "User with email  '{$user->email}' was successfully registered";
+
         return $this->sendResponse($success, $message);
     }
 
