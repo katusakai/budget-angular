593e8ce07b2412555fde3f131aa1a462b29e0a71
Tadas Janca <tadasjanca@gmail.com>
2020-02-24 20:40:16 +0200

    added some admin roles and it's seeder

---
+++ backend/database/seeds/RolesPermissionsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $super_admin_role        = Role::create(['name' => 'Super Admin']);
+        $edit_globals_permission = Permission::create(['name' => 'edit globals']);
+        $edit_users_permission2  = Permission::create(['name' => 'edit users']);
+        $super_admin_role->givePermissionTo($edit_globals_permission);
+        $super_admin_role->givePermissionTo($edit_users_permission2);
+
+        $admin_role = Role::create(['name' => 'admin']);
+        $admin_role->givePermissionTo($edit_users_permission2);
+    }
+}
