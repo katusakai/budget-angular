3a5955073f62d367ead02abeefd3965f50601a54
Tadas Janca <tadasjanca@gmail.com>
2020-03-05 20:18:09 +0200

    role update fix

--- frontend/src/app/services/auth/roles.service.ts
+++ frontend/src/app/services/auth/roles.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { RolesEventService } from "../../events/roles-event.service";
 
 @Injectable({
   providedIn: 'root'
@@ -7,33 +10,37 @@ import { AuthService } from "./auth.service";
 export class RolesService {
 
   private readonly userRoles: string;
+  private oldRoles;
 
   constructor(
-      private Auth: AuthService
+      private Auth: AuthService,
+      private RoleEvent: RolesEventService,
   ) {
     this.userRoles = 'user_roles';
   }
 
-  private set() {
-    this.Auth.getCurrentRoles().subscribe( data => {
-      let roles = data;
-      if (!roles[0]) {
-        localStorage.setItem(this.userRoles, JSON.stringify(['no roles']));
-        return;
-      }
-      localStorage.setItem(this.userRoles, JSON.stringify(roles));
-    })
+  public get(): Observable<any> {
+    return new Observable(observer => {
+      observer.next(localStorage.getItem(this.userRoles));
+    }).pipe(
+        tap(() => {
+          this.set();
+        }),
+    );
   }
 
-  public get(): Array<string> {
-    this.set();
-    // @ts-ignore
-    return localStorage.getItem(this.userRoles);
+  private set() {
+    this.Auth.getCurrentRoles().subscribe( data => {
+
+      if (JSON.stringify(data) !== JSON.stringify(this.oldRoles)) {
+        this.oldRoles = data;
+        localStorage.setItem(this.userRoles, JSON.stringify(data));
+        dispatchEvent(this.RoleEvent.event);
+      }
+    })
   }
 
   public remove() {
     localStorage.removeItem(this.userRoles);
   }
-
-
 }
--- frontend/src/app/services/auth/user-data.service.ts
+++ frontend/src/app/services/auth/user-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { IUser } from '../../models';
-import {RolesService} from "./roles.service";
+import { RolesService } from "./roles.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,29 +10,36 @@ export class UserDataService {
   public loggedIn: boolean;
   public user: IUser;
   public roles: Array<string>;
-  private roleSetter: number;
 
   constructor(
       private Auth: AuthService,
-      private Roles: RolesService
+      private Roles: RolesService,
   ) {
-    this.Auth.status.subscribe(
-        value => {
-          this.loggedIn = value;
-          if (this.loggedIn) {
-            this.Auth.getCurrentUser().subscribe(data  => this.user  = data);
+      addEventListener('role-update', () => {
+          this.getRoles();
+      });
+    this.set();
+  }
 
-            if (!(this.roles = this.Roles.get()) || this.roles.includes('no-roles')) {
-               this.roleSetter = setInterval( () => this.roles = this.Roles.get(),500);
-            }
-
-          } else {
-            delete this.user;
-            delete this.roles;
-            this.Roles.remove();
-            clearInterval(this.roleSetter)
+  private set() {
+      this.Auth.status.subscribe(
+          value => {
+              this.loggedIn = value;
+              if (this.loggedIn) {
+                  this.Auth.getCurrentUser().subscribe(data  => this.user  = data);
+                this.getRoles();
+              } else {
+                  delete this.user;
+                  delete this.roles;
+                  this.Roles.remove();
+              }
           }
-        }
-    );
+      );
+  }
+
+  private getRoles(){
+      this.Roles.get().subscribe(data => {
+          this.roles = data;
+      });
   }
 }
