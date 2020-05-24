a9374b40c89aa31a99d0a6a1974882b6c2a0be77
Tadas Janca <tadasjanca@gmail.com>
2020-05-24 22:23:19 +0300

    written remaining install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,15 +7,27 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-echo 'Running composer install'
+echo 'Running composer install...'
 docker-compose -f docker-compose-helpers.yml run --rm composer install
 
-#cd frontend && npm install && cd ..
-#chown -R $USER:$USER .
-#cp backend/.env.example backend/.env
-#cp .env.example .env
-#cp frontend/src/environments/env.ts.example frontend/src/environments/env.ts
-#docker-compose up -d --build
-#docker-compose exec backend php artisan key:generate
-#docker-compose exec backend php artisan config:cache
-#docker-compose exec backend php artisan migrate
+echo 'Running npm install...'
+docker-compose -f docker-compose-helpers.yml run --rm node npm install
+
+echo 'Making copies of .env files...'
+cp backend/.env.example backend/.env
+cp .env.example .env
+cp frontend/src/environments/env.ts.example frontend/src/environments/env.ts
+
+echo 'Building images...'
+docker-compose build --no-cache
+
+echo "Preparing container for data seeding"
+docker-compose up -d api backend db
+
+echo "Seeding default database data"
+docker-compose exec backend php artisan key:generate
+docker-compose exec backend php artisan config:cache
+docker-compose exec backend php artisan migrate:fresh --seed
+
+echo "Turning down containers"
+docker-compose down
