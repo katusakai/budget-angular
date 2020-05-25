7dff20a4845c00c05d1d8e54813162a93524215b
Tadas Janca <tadasjanca@gmail.com>
2020-05-25 19:13:05 +0300

    bug fix

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sh ./scripts/generate-cert-include.sh
 
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
@@ -39,44 +38,46 @@ docker-compose run --rm --entrypoint "\
     -subj '/CN=localhost'" certbot
 echo
 
+if [ "$1" = "prod" ]; then
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d frontend_prod
-echo
+  echo "### Starting nginx ..."
+  docker-compose up --force-recreate -d frontend_prod
+  echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+  echo "### Deleting dummy certificate for $domains ..."
+  docker-compose run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domains && \
+    rm -Rf /etc/letsencrypt/archive/$domains && \
+    rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+  echo "### Requesting Let's Encrypt certificate for $domains ..."
+  #Join $domains to -d args
+  domain_args=""
+  for domain in "${domains[@]}"; do
+    domain_args="$domain_args -d $domain"
+  done
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
+  # Select appropriate email arg
+  case "$email" in
+    "") email_arg="--register-unsafely-without-email" ;;
+    *) email_arg="--email $email" ;;
+  esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+  # Enable staging mode if needed
+  if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
+  docker-compose run --rm --entrypoint "\
+    certbot certonly --webroot -w /var/www/certbot \
+      $staging_arg \
+      $email_arg \
+      $domain_args \
+      --rsa-key-size $rsa_key_size \
+      --agree-tos \
+      --force-renewal" certbot
+  echo
 
-echo "### Reloading nginx ..."
-docker-compose exec frontend_prod nginx -s reload
+  echo "### Reloading nginx ..."
+  docker-compose exec frontend_prod nginx -s reload
+fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,31 +3,47 @@
 sh ./scripts/generate-cert-include.sh
 
 if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+  echo '### Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
-echo 'Running composer install...'
+sh ./scripts/init-letsencrypt.sh $1
+
+echo '### Running composer install...'
 docker-compose -f docker-compose-helpers.yml run --rm composer install
 
-echo 'Running npm install...'
+echo '### Running npm install...'
 docker-compose -f docker-compose-helpers.yml run --rm node npm install
 
-echo 'Making copies of .env files...'
-cp backend/.env.example backend/.env
-cp .env.example .env
-cp frontend/src/environments/env.ts.example frontend/src/environments/env.ts
-
-echo 'Building images...'
+echo '### Building images...'
 docker-compose build --no-cache
 
-echo "Preparing container for data seeding"
+echo "### Preparing container for data seeding"
 docker-compose up -d api backend db
 
-echo "Seeding default database data"
-docker-compose exec backend php artisan key:generate
-docker-compose exec backend php artisan config:cache
-docker-compose exec backend php artisan migrate:fresh --seed
+echo "### Seeding default database data"
+if [ "$OS" = "Windows_NT" ];
+then
+  winpty docker-compose exec backend php artisan key:generate
+  winpty docker-compose exec backend php artisan config:cache
+  winpty docker-compose exec backend php artisan migrate:fresh --seed
 
-echo "Turning down containers"
+  if [ "$1" != 'prod' ];
+  then
+    winpty docker-compose exec backend php artisan db:seed --class=FakeDataSeeder
+  fi
+
+else
+  docker-compose exec backend php artisan key:generate
+  docker-compose exec backend php artisan config:cache
+  docker-compose exec backend php artisan migrate:fresh --seed
+
+  if [ "$1" != 'prod' ];
+  then
+    docker-compose exec backend php artisan db:seed --class=FakeDataSeeder
+  fi
+
+fi
+
+echo "### Turning down containers"
 docker-compose down
