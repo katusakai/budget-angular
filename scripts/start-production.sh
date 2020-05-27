1de4aaf519275eeab887aeeaeb0814959f1edb80
Tadas Janca <tadasjanca@gmail.com>
2020-05-28 00:37:27 +0300

    added extra echo lines for start-production.sh script

--- scripts/start-production.sh
+++ scripts/start-production.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 WEB_PROD_SSL_PORT=$(grep WEB_PROD_SSL_PORT .env | cut -d '=' -f2)
-
+DOMAINLIST=($(grep DOMAINLIST .env | cut -d '=' -f2))
 
 echo '### Starting services for production'
 docker-compose up -d frontend_prod api backend db certbot
 
 echo '### Services for production are ready'
-echo "### Your production app is reachable by https://localhost:$WEB_PROD_SSL_PORT"
+echo "### Your production app is      reachable by https://localhost:$WEB_PROD_SSL_PORT"
+
+for domain in "${DOMAINLIST[@]}";
+do
+echo "### Your production app is also reachable by https://$domain"
+done
