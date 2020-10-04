20db6f172f732eaf511862bd01b6c253748fddf9
Tadas Janca <tadasjanca@gmail.com>
2020-10-04 21:15:32 +0300

    updated indexes

--- backend/database/migrations/2020_10_04_170309_alter_tables_indexes_and_names.php
+++ backend/database/migrations/2020_10_04_170309_alter_tables_indexes_and_names.php
@@ -17,6 +17,18 @@ class AlterTablesIndexesAndNames extends Migration
         Schema::rename('categories', 'category');
         Schema::rename('sub_categories', 'sub_category');
         Schema::rename('configurations', 'configuration');
+
+        Schema::table('category', function (Blueprint $table) {
+            $table->unique('name');
+        });
+
+        Schema::table('sub_category', function (Blueprint $table) {
+            $table->unique(['category_id', 'name']);
+        });
+
+        Schema::table('money_transaction', function (Blueprint $table) {
+            $table->index(['user_id', 'created_at']);
+        });
     }
 
     /**
@@ -26,6 +38,18 @@ class AlterTablesIndexesAndNames extends Migration
      */
     public function down()
     {
+        Schema::table('money_transaction', function (Blueprint $table) {
+            $table->dropIndex(['user_id', 'created_at']);
+        });
+
+        Schema::table('sub_category', function (Blueprint $table) {
+            $table->dropUnique(['category_id', 'name']);
+        });
+
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropUnique(['name']);
+        });
+
         Schema::rename('money_transaction', 'money_flows');
         Schema::rename( 'category', 'categories');
         Schema::rename('sub_category','sub_categories');
